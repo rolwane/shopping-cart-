5d27cd16d627a9225b848b3a2bab19641b7ecd2f
Rolwane Borges <rolwane@hotmail.com>
2021-12-03 14:54:30 -0300

    Correção de erros

--- script.js
+++ script.js
@@ -1,3 +1,47 @@
+const cartItems = document.querySelector('.cart__items');
+
+function getTotalPrice() {
+  const items = cartItems.querySelectorAll('li');
+  let totalPrice = 0;
+  items.forEach((item) => {
+    const itemPrice = item.getAttribute('data-price');
+    totalPrice += Number(itemPrice);
+  });
+  document.querySelector('.total-price').innerText = totalPrice;
+}
+
+function removeItem(event) {
+  event.target.remove();
+  saveCartItems();
+  getTotalPrice();
+}
+
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
+
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.setAttribute('data-sku', sku);
+  li.setAttribute('data-price', salePrice);
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', removeItem);
+  return li;
+}
+
+// Adiciona itens no carrinho
+function addToCart(event) {
+  if (event.target.className === 'item__add') {
+    const skuItem = getSkuFromProductItem(event.path[1]);
+    fetchItem(skuItem).then(({ id: sku, title: name, price: salePrice }) => {
+      cartItems.appendChild(createCartItemElement({ sku, name, salePrice }));
+      saveCartItems();
+      getTotalPrice();
+    });
+  }
+}
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -15,7 +59,7 @@ function createCustomElement(element, className, innerText) {
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
-
+  section.addEventListener('click', addToCart);
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
@@ -24,72 +68,22 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
-
-function getTotalPrice() {
-  const ol = document.querySelector('.cart__items');
-  const h2 = document.querySelector('.total-price');
-  const items = ol.querySelectorAll('li');
-
-  let totalPrice = 0;
-
-  items.forEach((item) => {
-    const itemPrice = item.getAttribute('data-price');
-    totalPrice += Number(itemPrice);
-  });
-  h2.innerText = totalPrice;
-}
-
-function cartItemClickListener(event) {
-  event.target.remove();
-  saveCartItems();
-  getTotalPrice();
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.setAttribute('data-sku', sku);
-  li.setAttribute('data-price', salePrice);
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
-
-function addToCart(event) {
-  if (event.target.className === 'item__add') {
-    const cart = document.querySelector('.cart__items');
-    const skuItem = getSkuFromProductItem(event.path[1]);
-    fetchItem(skuItem).then(({ id: sku, title: name, price: salePrice }) => {
-      cart.appendChild(createCartItemElement({ sku, name, salePrice }));
-      saveCartItems();
-      getTotalPrice();
-    });
-  }
-}
-
+// Limpa o carrinho
 document.querySelector('.empty-cart').addEventListener('click', () => {
   localStorage.cartItems = '[]';
-  document.querySelector('.cart__items').innerHTML = '';
-  getTotalPrice()
-})
+  cartItems.innerHTML = '';
+  getTotalPrice();
+});
 
 window.onload = () => {
+  // Atualiza o carrinho de compra caso tenha itens salvos
   getSavedCartItems(fetchItem, createCartItemElement, getTotalPrice);
-  
-  function addListener() {
-    const products = document.querySelectorAll('.item');
-    products.forEach((product) => product.addEventListener('click', addToCart));
-  }
 
+  // Lista os produtos na tela
   fetchProducts().then((response) => {
     const items = document.querySelector('.items');
     response.results.forEach(({ id: sku, title: name, thumbnail: image }) => {
       items.appendChild(createProductItemElement({ sku, name, image }));
     });
-
-    addListener();
   });
 };
